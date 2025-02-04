ef615a33e8fcb7c8c5b8f3807f8a145fe4a77e3e
Manuel Espinosa <106558272+manuelesvi@users.noreply.github.com>
2025-02-04 09:47:01 -0600

    Update pwd_gen.sh

--- pwd_gen.sh
+++ pwd_gen.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#passwords=($(pwgen -y 16 5))
-#for ((i=0; i < ${#passwords[@]}; i++))
+# This script requires pwgen command installed on your local environment.
+# It will generate 5 random passwords and let use pick one from the menu.
 
 passwords=$(pwgen -y 16 5)
 select pwd in $passwords
 do
     #echo $((i+1)). ${passwords[i]}
-    echo "You picked password #($REPLY):"
-    echo $pwd
+    echo "You picked password #($REPLY):"    
+    LAST_PWD=$pwd
+    echo "LAST_PWD = $LASTPWD"
     break;
 done
 
