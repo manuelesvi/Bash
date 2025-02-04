9ffb6ddd571976cbc383c4b7f3a280228fd245d4
Manuel Espinosa <manuel.espinosa@live.com>
2025-02-04 10:25:57 -0600

    comments

--- pwd_gen.sh
+++ pwd_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script requires pwgen command installed on your local environment.
-# It will generate 5 random passwords and let use pick one from the menu.
+# It will generate 5 random passwords and let the user pick one from the menu.
 
 passwords=$(pwgen -y 16 5)
 select pwd in $passwords
